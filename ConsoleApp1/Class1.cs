e8044c1e137f44a929e2ef5746747782a9a5bc36
Aleguid97 <aleguid97@gmail.com>
2024-01-31 15:55:59 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contocorrente
+{
+    internal class Conto
+    {
+        public string CCesiste { get; set; }
+        public int Prelievo { get; set; }
+        public int Versamento { get; set; }
+        public int Patrimonio { get; set; }
+
+        public string CheckAccess()
+        {
+            if (CCesiste == "yes")
+            {
+                return "Benvenuto sig. Guida";
+            }
+
+
+            else
+            {
+                return "Non possiede nessun conto in questa banca";
+            }
+        }
+    }
+
+        
